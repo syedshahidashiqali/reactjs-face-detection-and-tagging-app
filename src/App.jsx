0c266d108b7b7efa4f99d91f0cd7f7533fb6ece8
Syed Shahid <syedshahid1215@gmail.com>
2021-12-01 19:41:50 +0500

    add picture and draw detection on them but it is not on perfect location

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import { useRef, useEffect } from 'react';
 import './App.css';
+import * as faceapi from "face-api.js";
 
 function App() {
+
+  const imgRef = useRef();
+  const canvasRef = useRef();
+
+  const handleImage = async() => {
+    // async because face detection takes time
+    const detections = await faceapi
+      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(imgRef.current);
+      faceapi.draw.drawDetections(canvasRef.current, detections)
+  }
+
+  useEffect(() => {
+    const loadModels = () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      ])
+        .then(handleImage)
+        .catch(err => console.log(err))
+    }
+    imgRef.current && loadModels();
+  }, [])
   return (
     <div className="app">
-      Hello, world
+      <img 
+        ref={imgRef}
+        src="https://www.rivermendhealth.com/wp-content/uploads/2018/05/smiling-faces.jpg"
+        alt=""
+        width="940"
+        height="650"
+        crossOrigin="anonymous"
+      />
+      <canvas ref={canvasRef} width={940} height={650} />
     </div>
   );
 };
