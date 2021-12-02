fb12cca8f64bfe61974855bbc142afbf28a5ccca
Syed Shahid <syedshahid1215@gmail.com>
2021-12-02 17:28:09 +0500

    modify title and somy stylings

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
     return (
         <div className="navbar">
             <a href="/" style={{textDecoration: "none", color: "white"}}>
-                <span>MySocial</span>
+                <span>Face Detection App by Shahid</span>
             </a>
         </div>
     );
