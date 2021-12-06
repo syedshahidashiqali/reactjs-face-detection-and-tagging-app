e46c53830ef6773cab3f2564acb5d9af972aa130
Syed Shahid <syedshahid1215@gmail.com>
2021-12-06 14:04:45 +0500

    add icons locally after download because some was not showing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import NewPost from "./components/NewPost";
 
+// import icons
+import addImageIcon from "./icons/add-image-icon.png"
+import mapIcon from "./icons/map-icon.jpg"
+import calenderIcon from "./icons/calender-icon.svg"
+
 function App() {
 
   const [file, setFile] = useState();
@@ -26,7 +31,7 @@ function App() {
     // file && console.log(URL.createObjectURL(file))
   }, [file]);
 
-  console.log(image)
+  // console.log(image)
 
   return (
     <div>
@@ -48,17 +53,17 @@ function App() {
               <label htmlFor="file">
                   <img
                     className="addImg"
-                    src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                    src={addImageIcon}
                     alt=""
                   />
                   <img
                     className="addImg"
-                    src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                    src={mapIcon}
                     alt=""
                   />
                   <img
                     className="addImg"
-                    src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                    src={calenderIcon}
                     alt=""
                   />
               <button>Send</button>
